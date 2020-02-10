36575d10c406dea091c368360c662feea2a6a228
Marianne Barbosa <32180262+mariannebt@users.noreply.github.com>
2020-02-10 10:12:06 -0300

    First commit - entity
    First commit - entity

---
+++ Entity/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class Activity
+    {
+        #region Attributes
+        public int IdActivity { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public DateTime TimeTotal { get; set; }
+        public Agenda Agenda { get; set; }
+        #endregion
+
+        #region Constructor
+        public Activity()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Entity/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Entity
+{
+    public class Person
+    {
+        #region Attributes
+        public int IdPerson { get; set; }
+        public string Name { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Profile JobProfile { get; set; }
+        public Person Manager { get; set; }
+        public Job Role { get; set; }
+        public Status Status { get; set; }
+        public OfficeHours WorkHours { get; set; }
+        public Agenda Schedule { get; set; }
+        #endregion
+
+        #region Constructor
+        public Person()
+        {
+
+        }
+        #endregion
+
+    }
+}
